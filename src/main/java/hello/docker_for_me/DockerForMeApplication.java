43b4d240e2d8b33efd60f8c7ca254b3e96b75d38
SmS <mongddangand@likelion.org>
2025-04-20 10:22:42 +0900

    first commit

---
+++ src/main/java/hello/docker_for_me/DockerForMeApplication.java
@@ -0,0 +1,13 @@
+package hello.docker_for_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerForMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerForMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/docker_for_me/DockerForMeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.docker_for_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerForMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
